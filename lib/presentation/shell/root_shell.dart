1fee2b375b662d51ffbd505c92ec18c7434d6b62
David <deibeeed@deibeeeds-raputapu-tsuu.local>
2026-05-24 14:43:03 +0800

    feat(presentation): add RootShell with NavigationBar + IndexedStack

---
+++ lib/presentation/shell/root_shell.dart
@@ -0,0 +1,40 @@
+import 'package:ad_ranking_prototype/presentation/feed/view/feed_page.dart';
+import 'package:ad_ranking_prototype/presentation/liked/view/liked_page.dart';
+import 'package:flutter/material.dart';
+
+class RootShell extends StatefulWidget {
+  const RootShell({super.key});
+
+  @override
+  State<RootShell> createState() => _RootShellState();
+}
+
+class _RootShellState extends State<RootShell> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [FeedPage(), LikedPage()],
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (i) => setState(() => _currentIndex = i),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.favorite_outline),
+            selectedIcon: Icon(Icons.favorite),
+            label: 'Liked',
+          ),
+        ],
+      ),
+    );
+  }
+}
